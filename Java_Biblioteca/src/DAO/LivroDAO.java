37698c0f667e4efd7cf2a1f6f24feb1c2400475f
Flavio Nascimento <54750750+FlavioNascimento99@users.noreply.github.com>
2024-12-04 12:16:44 -0300

    🧠 atualização na regra de negócio - DAO

--- Java_Biblioteca/src/DAO/LivroDAO.java
+++ Java_Biblioteca/src/DAO/LivroDAO.java
@@ -12,26 +12,26 @@ public class LivroDAO {
 
     // Construtor do DAO - Recebe a conexão com o banco de dados (ObjectContainer)
     public LivroDAO(ObjectContainer database) {
-        this.database = database;    // Injeta o Banco de Dados (ObjectContainer) no DAO de Livro.
+        this.database = database;          // Injeta o Banco de Dados (ObjectContainer) no DAO de Livro.
     }
 
     // Método para salvar um objeto Livro no banco de dados
     public void salvar(Livro livro) {
-        database.store(livro);        // Armazena o objeto Livro no banco de dados.
+        database.store(livro);             // Armazena o objeto Livro no banco de dados.
     }
 
     // Método para listar todos os livros armazenados no banco
     public List<Livro> listarTodos() {
         Query query = database.query();    // Inicia uma consulta no db4o por meio do seu método query().
-        query.constrain(Livro.class);       // Restringe a consulta para buscar apenas objetos do tipo Livro.
+        query.constrain(Livro.class);      // Restringe a consulta para buscar apenas objetos do tipo Livro.
         return query.execute();            // Executa a consulta e retorna a lista de objetos Livro encontrados.
     }
 
     // Método para buscar livros pelo título
     public List<Livro> buscaPorTitulo(String titulo) {
         Query query = database.query();    // Inicia uma nova consulta.
-        query.constrain(Livro.class);       // Define que queremos buscar objetos do tipo Livro.
-        query.descend("titulo")             // Acessa o atributo 'titulo' de cada objeto Livro.
+        query.constrain(Livro.class);      // Define que queremos buscar objetos do tipo Livro.
+        query.descend("titulo")            // Acessa o atributo 'titulo' de cada objeto Livro.
              .constrain(titulo);           // Filtra a consulta para encontrar livros com o título correspondente ao parâmetro passado.
         return query.execute();            // Executa a consulta e retorna os livros encontrados que atendem ao critério de título.
     }
--- Java_Biblioteca/src/DAO/VendaDAO.java
+++ Java_Biblioteca/src/DAO/VendaDAO.java
@@ -14,5 +14,29 @@ public class VendaDAO {
 	public void salvar(Venda venda) {
 		database.store(venda);
 	}
+
+	public List<Venda> listarTodas() {
+		Query query = database.query();	// Inicia uma query (consulta)
+		query.constrain(Venda.class);	// Definido tipo de objeto desejado na query
+		return query.execute();			// Executa a query
+	}
 	
+	public Venda buscarPorId(int id) {	
+		Query query = database.query();				  		  // Inicia consulta
+		query.constrain(Venda.class);				  		  // Define tipo de objeto desejado na query
+		query.descend("id").constrain(id);					  // Filtragem do objeto a partir do campo "id"
+		List<Venda> resultado = query.execute();			  // Executa a consulta
+		return resultado.isEmpty() ? null : resultado.get(0); // Retorna venda ou nulo ao fim do método.
+	}
+
+	public List<Venda> listaCliente(Cliente cliente) {
+		Query query = database.query();
+		query.constrain(Venda.class);
+		query.descend("cliente").constrain(cliente);
+		return query.execute();
+	}
+
+	public void deletar(Venda venda) {
+        database.delete(venda); // Remove a venda do banco de dados
+    }
 }
